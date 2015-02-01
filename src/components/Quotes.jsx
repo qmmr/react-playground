18f088a839252c3c9e8b1523ada9dd3aba63ef16
Marcin Kumorek <kumeek@gmail.com>
2015-02-01 23:03:14 +0000

    [master] Update Quotes, add prevStep and nextStep methods

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -7,7 +7,7 @@ import QuotesList from './QuotesList.jsx'
 
 export default React.createClass({
 
-	name: 'Quotes',
+	displayName: 'Quotes',
 
 	getInitialState() {
 		return {
@@ -25,7 +25,22 @@ export default React.createClass({
 				<h2>Quotes</h2>
 				<QuotesList quotes={ this.state.quotes } />
 				<QuotesForm onSubmit={ this.onSubmitQuote } />
+				<button className="btn btn-primary" onClick={ this._prevStep }>Prev step</button>
+				<button className="btn btn-primary pull-right" onClick={ this._nextStep }>Next step</button>
 			</div>
 		)
+	},
+
+	_prevStep(e) {
+		e.preventDefault()
+
+		this.props.prevStep()
+	},
+
+	_nextStep(e) {
+		e.preventDefault()
+
+		// this.props.saveValues(data)
+		this.props.nextStep()
 	}
 })
