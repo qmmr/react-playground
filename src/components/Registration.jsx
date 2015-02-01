2791056929149af5085e3896fe203e970df8d861
Marcin Kumorek <kumeek@gmail.com>
2015-02-01 23:03:56 +0000

    [master] Refactor Registration and Survey components

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -8,7 +8,7 @@ import Survey from './Survey.jsx'
 import Results from './Results.jsx'
 // import Confirmation from './Confirmation.jsx'
 // import Success from './Success.jsx'
-import Timer from './Timer.jsx'
+// import Timer from './Timer.jsx'
 import Quotes from './Quotes.jsx'
 
 var fieldValues = {
@@ -21,29 +21,10 @@ var fieldValues = {
 
 export default React.createClass({
 
-	name: 'Registration',
+	displayName: 'Registration',
 
 	getInitialState() {
 		return { step: 1 }
-	},
-
-	render() {
-		let progressValue = this.state.step / 4 * 100
-		let percentage = `${progressValue}%`
-		let style = { width: percentage }
-
-		return (
-			<section className="row">
-				<h3>Step: { this.state.step }</h3>
-				<Timer />
-				<div className="progress">
-					<div className="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow={ progressValue } aria-valuemin="0" aria-valuemax="100" style={ style }>
-						<span className="sr-only">{ percentage } Complete (success)</span>
-					</div>
-				</div>
-				{ this._showStep() }
-			</section>
-		)
 	},
 
 	saveValues(values) {
@@ -70,19 +51,32 @@ export default React.createClass({
 		this.setState({ step: 1 })
 	},
 
+	render() {
+		let progressValue = this.state.step / 4 * 100
+		let percentage = `${progressValue}%`
+		let style = { width: percentage }
+
+		return (
+			<section className="row">
+				<h3>Step: { this.state.step }</h3>
+				<div className="progress">
+					<div className="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow={ progressValue } aria-valuemin="0" aria-valuemax="100" style={ style }>
+						<span className="sr-only">{ percentage } Complete (success)</span>
+					</div>
+				</div>
+				{ this._showStep() }
+			</section>
+		)
+	},
+
 	_showStep() {
 		switch(this.state.step) {
 			case 1:
-				return <Quotes
+				return <AccountFields
 						fieldValues={ fieldValues }
 						nextStep={ this.nextStep }
 						saveValues={ this.saveValues } />
-			// case 1:
-			// 	return <AccountFields
-			// 			fieldValues={ fieldValues }
-			// 			nextStep={ this.nextStep }
-			// 			saveValues={ this.saveValues } />
-			// 	break
+				break
 			case 2:
 				return <Survey
 						fieldValues={ fieldValues }
@@ -90,6 +84,12 @@ export default React.createClass({
 						prevStep={ this.prevStep }
 						saveValues={ this.saveValues } />
 			case 3:
+				return <Quotes
+						fieldValues={ fieldValues }
+						nextStep={ this.nextStep }
+						prevStep={ this.prevStep }
+						saveValues={ this.saveValues } />
+			case 4:
 				return <Results
 						fieldValues={ fieldValues }
 						prevStep={ this.prevStep }
