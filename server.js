3b1456ca9d3050a05d05492ba98af007ec9ad69b
Marcin Kumorek <marcin.kumorek@gamesys.co.uk>
2015-04-04 23:11:30 +0100

    [webpack] Starting point for new playground with react 0.13 and webpack with hot-loader

---
+++ server.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+var HOST = 'localhost'
+var PORT = 8000
+
+new WebpackDevServer(webpack(config), {
+	contentBase: 'app',
+	publicPath: config.output.publicPath,
+	hot: true,
+	historyApiFallback: true
+}).listen(PORT, HOST, function (err, result) {
+	if (err) {
+		console.log(err);
+	}
+
+	console.log('WebpackDevServer is listening at localhost:' + PORT);
+});
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+	devtool: 'eval',
+	entry: [
+		'webpack-dev-server/client?http://localhost:8000', // WebpackDevServer host and port
+		'webpack/hot/only-dev-server',
+		'./app/index' // Your appʼs entry point
+	],
+	output: {
+		path: path.join(__dirname + 'app'),
+		filename: 'bundle.js',
+		publicPath: '/app/'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	],
+	resolve: {
+		extensions: [ '', '.js', '.jsx' ]
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+			{ test: /\.jsx$/, loaders: [ 'react-hot', 'babel' ], include: path.join(__dirname, 'app') },
+			{ test: /\.html$/, loader: 'raw', exclude: /node_modules/ },
+			{ test: /\.css$/, loader: 'style!css', exclude: /node_modules/ }
+		]
+	}
+}
