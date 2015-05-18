319a1731a7dcc3be548d380e0016785154501b94
Marcin Kumorek <kumeek@gmail.com>
2015-05-18 23:21:48 +0100

    [child-context] Playing with childContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,77 @@
 import React from 'react'
 import User from './User.jsx'
 
-//var App = React.createClass({
-	//getDefaultProps: function () {
-		//return {
-			//title: 'React is awesome!'
-		//}
-	//},
+var json = '{"html":"<h1>Hello world!</h1>","name":"Foo"}'
 
-	//render: function () {
-		//return (
-			//<div className='jumbotron'>
-				//<h1>React.js v0.13 with webpack hot-loader</h1>
-				//<div className='well'>{ this.props.title }</div>
-				//<Badge amount={ 42 } />
-			//</div>
-		//)
-	//}
-//})
+var App = React.createClass({
+	getInitialState: function() {
+		return { name: '' }
+	},
 
-class App extends React.Component {
-	constructor(props) {
-		super(props)
+	childContextTypes: {
+		setName: React.PropTypes.func.isRequired
+	},
 
-		this.state = { title: props.title }
-	}
+	getChildContext: function() {
+		return {
+			setName: name => this.setState({ name })
+		}
+	},
 
-	render() {
+	componentDidMount() {
+		let header = React.findDOMNode(this.refs.header)
+		console.log('cMARCIN :: App.jsx:23 :: child', header)
+	},
+
+	render: function () {
 		return (
 			<div className='jumbotron'>
-				<div className='row'>
-					<User login='qmmr' />
-					<User login='cesarenaldi' />
-					<User login='wycats' />
-				</div>
+				<h1 ref='header'>React.js v0.13 with webpack hot-loader</h1>
+				<div>Hi, my name is { this.state.name }!</div>
+				{ JSON.parse(json).html }
+				<Child />
 			</div>
 		)
 	}
-}
+})
 
+var Child = React.createClass({
+	contextTypes: {
+		setName: React.PropTypes.func.isRequired
+	},
+
+	updateName: function(e) {
+		this.context.setName(e.currentTarget.value)
+	},
+
+	render() {
+		return (
+			<input type='text' onChange={ this.updateName } value={ this.context.name } defaultValue={ 'Hello' } />
+		)
+	}
+})
+
+// class App extends React.Component {
+// 	constructor(props) {
+// 		super(props)
+
+// 		this.state = { title: props.title }
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className='jumbotron'>
+// 				<div className='row'>
+// 					<User login='qmmr' />
+// 					<User login='cesarenaldi' />
+// 					<User login='wycats' />
+// 				</div>
+// 			</div>
+// 		)
+// 	}
+// }
+
+App.displayName = 'App'
 App.defaultProps = { title: 'React is awesome!' }
 
 export default App
